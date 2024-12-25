04bc304d0521d16347de6484983ce827e760d8b1
Grablocker <2804027546@qq.com>
2024-12-25 16:33:00 +0800

    初步写好了SM4单块加密的骨架函数

---
+++ main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <cstdint>
+
+void sm4_key_expansion(const uint8_t *key, uint32_t *rk);
+void sm4_encrypt_block(const uint8_t *plaintext, uint8_t *ciphertext, const uint32_t *rk);
+void sm4_decrypt_block(const uint8_t *ciphertext, uint8_t *plaintext, const uint32_t *rk);
+uint32_t sm4_t_function(uint32_t input);
+uint32_t sm4_sbox_substitution(uint32_t input);
+uint32_t sm4_linear_transformation(uint32_t input);
